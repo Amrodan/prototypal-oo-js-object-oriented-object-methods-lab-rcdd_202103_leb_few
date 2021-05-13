f39b5fe96c80824eeb790ce2ce22be6eaf5a3a9b
Amro dandashli <amrdandashli@gmail.com>
2021-05-13 07:18:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ return 'I like to help people.'
     return 'You will see great things from Scuber.'
   }
   sayHi () {
-    return  `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return  `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
